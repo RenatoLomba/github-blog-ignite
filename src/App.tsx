4431896464f2ccf79c049061c2344e992cdba44f
Renato Lomba <rntlomba@gmail.com>
2022-08-20 18:58:06 -0300

    Cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { Box, Flex } from '@chakra-ui/react'
 
+import { Card } from './components/card'
 import { Link } from './components/link'
 import { TextInput } from './components/text-input'
 
 export function App() {
   return (
-    <Flex w="100%" padding={6} gap="4" flexDir="column">
+    <Flex w="100%" maxWidth="1312px" mx="auto" pt="10" gap="4" flexDir="column">
       <TextInput placeholder="Buscar conteúdo" />
 
       <Box>
         <Link>Github</Link>
       </Box>
+
+      <Flex w="100%" flexWrap="wrap" gap="8">
+        <Card />
+        <Card />
+        <Card />
+      </Flex>
     </Flex>
   )
 }
---
+++ src/components/card.tsx
@@ -0,0 +1,48 @@
+import { FC } from 'react'
+
+import { Box, Flex, Heading, Text } from '@chakra-ui/react'
+
+export const Card: FC = () => {
+  return (
+    <Box
+      maxWidth="416px"
+      w="100%"
+      h="260px"
+      bgColor="marine.600"
+      borderRadius="10px"
+      p={['6', '8']}
+    >
+      <Flex as="header" gap={['2', '4']} flexDir={['column', 'row']}>
+        <Heading fontSize="xl">
+          JavaScript data types and data structures
+        </Heading>
+        <Text as="span" color="marine.300" fontSize="sm" whiteSpace="pre">
+          Há 1 dia
+        </Text>
+      </Flex>
+
+      <Text
+        mt="5"
+        // whiteSpace="nowrap"
+        overflow="hidden"
+        text-overflow="ellipsis"
+        sx={{
+          display: '-webkit-box',
+          '-webkit-line-clamp': '4',
+          '-webkit-box-orient': 'vertical',
+        }}
+      >
+        Programming languages all have built-in data structures, but these often
+        differ from one language to another. This article attempts to list the
+        built-in data structures available in JavaScript and what properties
+        they have. These can be used to build other data structures. Wherever
+        possible, comparisons with other languages are drawn. Dynamic typing
+        JavaScript is a loosely typed and dynamic language. Variables in
+        JavaScript are not directly associated with any particular value type,
+        and any variable can be assigned (and re-assigned) values of all types:
+        let foo = 42; // foo is now a number foo; // foo is now a string fo //
+        foo is now a boolean
+      </Text>
+    </Box>
+  )
+}
